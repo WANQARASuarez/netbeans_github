cb05558bb941677c2a1966c2a9dc2830b063fdd5
WANQARASuarez <42620627+WANQARASuarez@users.noreply.github.com>
2018-08-22 15:20:41 -0500

    Update Net_gib.java

--- net_gib/src/net_gib/Net_gib.java
+++ net_gib/src/net_gib/Net_gib.java
@@ -19,6 +19,7 @@ public class Net_gib {
     public static void main(String[] args) {
         // TODO code application logic here
         JOptionPane.showMessageDialog(null, "HOLA");
+        JOptionPane.showMessageDialog(null, "MUNDO");
     }
     
 }
